9dec856423fcd44f6a9139a44f464f998b668980
Sven Havgen <sven_havgen@hotmail.com>
2018-12-17 16:12:38 +1100

    Add React repo

---
+++ TKN_STORE/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace TKN_STORE
+{
+    public static class Constants
+    {
+        public static string AuthorizationCookieKey => "Auth";
+        public static string HttpContextServiceUserItemKey => "ServiceUser";
+    }
+}
---
+++ TKN_STORE/Models/ServiceUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TKN_STORE.Models
+{
+    public class ServiceUser
+    {
+        public string Login { get; set; }
+    }
+}
